49db62e8f0239140b8dea655ea5ce31f4b534a95
BryanTorresCalle <50812831+BryanTorresCalle@users.noreply.github.com>
2021-04-13 20:08:07 -0500

    Se agregan las listas con numeros aleatorios y gráficas

--- factores_primos.py
+++ factores_primos.py
@@ -1,21 +1,42 @@
 import time
+import numpy
+import random
+import matplotlib.pyplot as plt
 
 def factoresPrimos(num):
 
     primos = []
     inicio = time.time()
-    print(inicio)
     for i in range(2,num + 1):
 
         while num % i == 0:
             primos.append(i)
             num = num / i
     fin = time.time()
-    print(fin)
     tiempo = float(fin - inicio)
     print("El tiempo de procesamiento fue:", tiempo)
-    return primos
+    print(primos)
+    return tiempo
 
 
-print(factoresPrimos(425869237))
 
+def genNumbers():
+
+    numbers = []
+    for i in range(0,12):
+        numbers.append(random.randint((10**i) , (10 ** (i+1)) ))
+    return numbers
+
+
+listNumbers = genNumbers()
+listTime = []
+for n in listNumbers:
+    print("para ", n, ":")
+    listTime.append(factoresPrimos(n))
+
+print("numeros: ", listNumbers)
+print("Tiempos:", listTime)
+
+digitos = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+plt.plot(digitos, listTime)
+plt.show()
